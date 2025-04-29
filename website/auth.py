d412ad8385cac3c2f9677abe8676c01c5d80ff78
SanskarLoganDev <devlogan70@gmail.com>
2025-04-28 22:40:19 -0400

    Added comments

--- website/auth.py
+++ website/auth.py
@@ -32,7 +32,7 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-# This is a method to sign up new customers
+# This is a method to sign up new customers/users
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
